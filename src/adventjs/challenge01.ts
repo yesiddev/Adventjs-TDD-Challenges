018c141236ca7232efe4d38e5cd4a502f4bcb908
yesiddev <yesiddeveloper@gmail.com>
2022-09-10 14:50:04 -0500

    first challenge

---
+++ src/adventjs/challenge01.ts
@@ -0,0 +1,42 @@
+/*
+Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color.
+Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y
+que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+*/
+
+interface ISheeps {
+  name: string;
+  color: string;
+}
+
+export const sheeps: ISheeps[] = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo' },
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo' },
+];
+
+// First Solution
+/* const filteredSheeps = (sheeps: ISheeps[]): ISheeps[] => {
+  const redSheeps = sheeps.filter(({ color }) => color === 'rojo');
+  const correctSheeps = redSheeps.filter(
+    ({ name }) =>
+      (name.includes('N') && name.includes('a')) ||
+      (name.includes('A') && name.includes('n')) ||
+      (name.includes('N') && name.includes('A')) ||
+      (name.includes('n') && name.includes('a'))
+  );
+  return correctSheeps;
+}; */
+
+// Refactoring Solution
+export const filteredSheeps = (sheeps: ISheeps[]): ISheeps[] => {
+  const redSheeps = sheeps.filter(({ color }) => color === 'rojo');
+  const correctSheeps = redSheeps.filter(({ name }) => {
+    const nameLowerCase = name.toLowerCase();
+    return nameLowerCase.includes('n') && nameLowerCase.includes('a');
+  });
+  return correctSheeps;
+};
